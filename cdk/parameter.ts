75a468fcb9b9909bc3672bfc5b011fb28ca1c6e5
Derek Gaston <friedmud@gmail.com>
2025-05-01 10:03:19 -0600

    Add dev prod default deployments

--- cdk/parameter.ts
+++ cdk/parameter.ts
@@ -6,3 +6,30 @@ export const bedrockChatParams = new Map<string, BedrockChatParametersInput>();
 
 // If you define "default" environment here, parameters in cdk.json are ignored
 // bedrockChatParams.set("default", {});
+
+// Define parameters for the default environment
+bedrockChatParams.set("default", {
+    bedrockRegion: "us-east-1",
+    selfSignUpEnabled: false,
+    enableBotStore: false,
+    autoJoinUserGroups: [],
+    tokenValidMinutes: 120,
+  });
+  
+  // Define parameters for additional environments
+  bedrockChatParams.set("dev", {
+    bedrockRegion: "us-east-1",
+    selfSignUpEnabled: false,
+    enableBotStore: true,
+    autoJoinUserGroups: [],
+    tokenValidMinutes: 120,
+  });
+  
+  bedrockChatParams.set("prod", {
+    bedrockRegion: "us-east-1",
+    selfSignUpEnabled: false,
+    enableBotStore: true,
+    autoJoinUserGroups: [],
+    tokenValidMinutes: 120,
+    enableLambdaSnapStart: true,
+  });
